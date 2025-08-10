7312f1c1bdb3c99b8e137b863486241991796687
srinivas <srigonepudi@gmail.com>
2025-08-10 10:59:38 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import logo from "../assets/logo.png"; // path to your logo image
 
 export default function Header() {
+  // "signup" is default active
+  const [active, setActive] = useState("signup");
+
   return (
     <header className="header">
       <a className="brand" href="#">
         <img className="brand__logo" src={logo} alt="SnapDocs logo" />
       </a>
       <div className="actions">
-        <button className="btn btn--ghost">Log in</button>
-        <button className="btn btn--brand">Sign up</button>
+        <button
+          onClick={() => setActive("login")}
+          className={`btn ${active === "login" ? "btn--brand" : "btn--ghost"}`}
+        >
+          Log in
+        </button>
+        <button
+          onClick={() => setActive("signup")}
+          className={`btn ${active === "signup" ? "btn--brand" : "btn--ghost"}`}
+        >
+          Sign up
+        </button>
       </div>
     </header>
   );
