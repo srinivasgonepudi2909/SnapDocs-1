3cff589090b88b946f162fe29e5dadfe233e24c7
srinivas <srigonepudi@gmail.com>
2025-08-10 11:23:37 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import HomePage from "./components/HomePage/HomePage"; // 👈 Import your homepage
+import HomePage from "./components/Homepage/Homepage.jsx"; // 👈 Import your homepage
 
 export default function App() {
   return (
