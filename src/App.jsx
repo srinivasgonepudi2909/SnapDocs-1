b56bf23f20070a2863756016bf018d99c5bea8d7
srinivas <srigonepudi@gmail.com>
2025-08-10 11:38:20 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header.jsx";
-import HomeIntro from "./pages/homeintro.jsx";
+import HomeIntro from "./pages/homeintro.jsx"; // match the lowercase file name
 
 export default function App() {
   return (
--- src/pages/HomeIntro.jsx
+++ src/pages/HomeIntro.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./homeIntro.css";
+import "./homeintro.css";
 
 function Folder({ icon, label }) {
   return (
