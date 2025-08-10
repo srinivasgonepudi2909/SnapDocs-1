a52faa6123434e03bcb26f33f43f8cb369bc861f
srinivas <srigonepudi@gmail.com>
2025-08-10 11:29:43 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
-import Homepage from "./components/Homepage/Homepage"; // ✅ Default export
 
 export default function App() {
   return (
     <>
       <Header />
-      <Homepage /> {/* ✅ Renders logo + intro section */}
+      {/* hero/sections go here */}
     </>
   );
 }
